ec15be1b89c27c2891ef94fb4b77e006d1678169
raven <laingraven.java@gmail.com>
2024-03-13 20:57:39 +0700

    addscreenshot gif

--- src/test/java/raven/swing/spinner/Demo.java
+++ src/test/java/raven/swing/spinner/Demo.java
@@ -15,22 +15,22 @@ public class Demo extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(UIScale.scale(new Dimension(500, 300)));
         setLocationRelativeTo(null);
-        JPanel panel = new JPanel(new MigLayout("fill, al center center","[center]"));
+        JPanel panel = new JPanel(new MigLayout("fill, al center center", "[center]"));
         add(panel);
-        SpinnerProgress sp1 = new SpinnerProgress(new FlatSVGIcon("raven/swing/spinner/save.svg"));
-        sp1.setHorizontalTextPosition(SwingConstants.CENTER);
-        sp1.setVerticalTextPosition(SwingConstants.BOTTOM);
-        sp1.setValue(70);
-        sp1.setStringPainted(true);
-        panel.add(sp1);
+        SpinnerProgress spinner = new SpinnerProgress(new FlatSVGIcon("raven/swing/spinner/save.svg"));
+        spinner.setHorizontalTextPosition(SwingConstants.CENTER);
+        spinner.setVerticalTextPosition(SwingConstants.BOTTOM);
+        spinner.setValue(70);
+        spinner.setStringPainted(true);
+        panel.add(spinner);
         add(panel);
 
         JButton button = new JButton("Test Action");
         button.addActionListener(e -> {
-            boolean act = !sp1.isIndeterminate();
-            sp1.setIndeterminate(act);
+            boolean act = !spinner.isIndeterminate();
+            spinner.setIndeterminate(act);
         });
-        panel.add(button,"dock south,gap 5 5 5 5");
+        panel.add(button, "dock south,gap 5 5 5 5");
     }
 
     public static void main(String[] args) {
@@ -38,6 +38,8 @@ public class Demo extends JFrame {
         FlatMacLightLaf.setup();
         UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
         UIManager.put("ProgressBar.cycleTime", 1000);
+        UIManager.put("ProgressBar.repaintInterval", 15);
+
         EventQueue.invokeLater(() -> new Demo().setVisible(true));
     }
 }
